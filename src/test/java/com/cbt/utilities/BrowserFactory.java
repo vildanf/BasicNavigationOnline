6dad79d2a5c30522d7a6e970d173ab06893c8574
vildanf <vildanf@yahoo.com>
2020-09-28 16:48:01 -0400

    first commit

---
+++ src/test/java/com/cbt/utilities/BrowserFactory.java
@@ -0,0 +1,16 @@
+package com.cbt.utilities;
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.openqa.selenium.WebDriver;
+
+public class BrowserFactory {
+    public static WebDriver getDriver(String browser) {
+
+    String os = System.getProperty("os.name");
+    return null;
+}
+
+//    public static void main(String[] args) {
+//        String os = System.getProperty("os.name");
+//        System.out.println(os);
+//    }
+}
